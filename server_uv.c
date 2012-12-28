e36f141d6a6a72bd9a5322c3f5fef9291415e099
Michael Neumann <mneumann@ntecs.de>
2012-12-28 12:36:52 -0600

    fix bug in libuv "server"

--- server_uv.c
+++ server_uv.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h> // malloc
 #include <string.h> // strlen
+#include <assert.h>
+
+#define LOG(msg) fprintf(stderr, msg "\n")
+
+#define LOG(msg)
 
 static uv_loop_t *loop;
 
@@ -16,31 +21,53 @@ struct write_req_t {
 };
 
 void my_close(uv_handle_t *handle) {
-  fprintf(stderr, "Closing handle\n");
+  LOG("Closing handle");
+  assert(handle);
+  free(handle); // IMPORTANT!!!
 }
 
-void my_callback(uv_write_t *req, int status) {
-  fprintf(stderr, "Write finished\n");
+static void write_finished(uv_write_t *req, int status) {
+  LOG("Write finished");
   struct write_req_t *wr = (struct write_req_t*) req;
-  free(wr->buf.base);
-  free(wr);
+  assert(req);
+  assert(wr);
+  assert(wr->buf.base);
+  assert(wr->stream);
 
   uv_close((uv_handle_t*)wr->stream, my_close);
+
+  free(wr->buf.base);
+  free(wr);
 }
 
 void my_read(uv_stream_t *stream, ssize_t nread, uv_buf_t buf) {
-  fprintf(stderr, "My read\n");
+  LOG("My read");
   if (nread == -1) {
+    // XXX: uv_read_stop implicit?
+    //printf("my read: nread == -1\n");
     uv_close((uv_handle_t*)stream, my_close);
   } else if (nread > 0) {
-    const char *str = "HTTP/1.1 OK 200\r\nConnection: Close\r\nContent-Type: text/html\r\nContent-Length: 39\r\n\r\n<html><body><b>Hallo</b></body></html>";
+    int err = uv_read_stop(stream);
+    assert(err == 0);
+
+    const char *str = "HTTP/1.1 200 OK\r\nConnection: Close\r\nContent-Type: text/html\r\nContent-Length: 39\r\n\r\n<html><body><b>Hallo</b></body></html>";
     const int sz = strlen(str);
 
     struct write_req_t *req = (struct write_req_t*) malloc(sizeof(struct write_req_t));
-    req->buf = uv_buf_init((char*) malloc(sz), sz);
+    assert(req);
+
+    char *b = (char*) malloc(sz);
+    assert(b);
+    req->buf = uv_buf_init(b, sz);
+
     req->stream = stream;
+    assert(req->buf.base);
     memcpy(req->buf.base, str, sz);
-    uv_write((uv_write_t*) req, stream, &req->buf, 1, my_callback);
+    err = uv_write((uv_write_t*) req, stream, &req->buf, 1, write_finished);
+    assert(err == 0);
+  } else {
+    assert(nread == 0);
+    printf("NREAD == 0\n");
   }
 
   if (buf.base) {
@@ -50,18 +77,31 @@ void my_read(uv_stream_t *stream, ssize_t nread, uv_buf_t buf) {
 
 void on_new_connection(uv_stream_t *server, int status) {
   if (status == -1) {
+    printf("on_new_connection: ERROR\n");
     // error!
     return;
   }
 
   uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
-  uv_tcp_init(loop, client);
+  assert(client);
+
+  int err;
+
+  err = uv_tcp_init(loop, client);
+  assert(err == 0);
+  err = uv_tcp_nodelay(client, 1);
+  assert(err == 0);
+
   if (uv_accept(server, (uv_stream_t*) client) == 0) {
-    uv_read_start((uv_stream_t*) client, alloc_buffer, my_read);
+    err = uv_read_start((uv_stream_t*) client, alloc_buffer, my_read);
+    assert(err == 0);
+  } else {
+    printf("ACCEPT failed\n");
+    free(client);
   }
-  else {
+  /*else {
     uv_close((uv_handle_t*) client, NULL);
-  }
+  }*/
 }
 
 int main(int argc, char **argv) {
